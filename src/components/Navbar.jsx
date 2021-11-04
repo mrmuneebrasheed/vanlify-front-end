5301b7968b0cb1a12a9cbee8c4079c40062196df
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-11-04 10:04:57 +0100

    branch added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <div className="nav">
+            <div className="skew">
+                <h1 className="title">Vanlify</h1>
+            </div>
+        </div>
+    );
+}
